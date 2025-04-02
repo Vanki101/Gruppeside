94fd8dbf5c6a44abc00d6eb2fa5a6c8e49efb9d2
Vanki101 <sipbani@hotmail.com>
2025-04-02 12:40:02 +0200

    Satt og sanity og react
    02.04.2025 sipan
    
    Ordnet sanity database og react. har ikke laget komponent mappe enda

---
+++ gruppeoppgave/studio/schemaTypes/index.js
@@ -0,0 +1,4 @@
+// studio/schemas/index.js
+import member from './member'
+
+export const schemaTypes = [member]
---
+++ gruppeoppgave/studio/schemaTypes/member.js
@@ -0,0 +1,74 @@
+// studio/schemas/member.js
+export default {
+    name: 'member',
+    title: 'Gruppemedlem',
+    type: 'document',
+    fields: [
+      {
+        name: 'firstName',
+        title: 'Fornavn',
+        type: 'string',
+        validation: Rule => Rule.required()
+      },
+      {
+        name: 'lastName',
+        title: 'Etternavn',
+        type: 'string',
+        validation: Rule => Rule.required()
+      },
+      {
+        name: 'email',
+        title: 'E-post',
+        type: 'string',
+        validation: Rule => Rule.required().email()
+      },
+      {
+        name: 'image',
+        title: 'Profilbilde',
+        type: 'image',
+        options: {
+          hotspot: true
+        }
+      },
+      {
+        name: 'biography',
+        title: 'Biografi',
+        type: 'text'
+      },
+      {
+        name: 'interests',
+        title: 'Interesser',
+        type: 'array',
+        of: [{ type: 'string' }]
+      },
+      {
+        name: 'logs',
+        title: 'Loggføringer',
+        type: 'array',
+        of: [
+          {
+            type: 'object',
+            fields: [
+              {
+                name: 'description',
+                title: 'Beskrivelse',
+                type: 'string',
+                validation: Rule => Rule.required()
+              },
+              {
+                name: 'date',
+                title: 'Dato',
+                type: 'datetime',
+                initialValue: () => new Date().toISOString()
+              },
+              {
+                name: 'hours',
+                title: 'Timer',
+                type: 'number'
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  }
